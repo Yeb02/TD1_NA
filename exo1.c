83f646fe387d76ab412b194413a4133eee93e498
Yann Blum <alpha.blum@gmail.com>
2021-06-01 15:29:52 +0200

    exo 1

---
+++ exo1.c
@@ -0,0 +1,12 @@
+int main()
+{
+    printf("Hello World!\n");
+    int a, b;
+    a = 4;
+    printf("a = %d\n", a);
+
+    for (int i = 0; i < 10; i++) {
+
+        printf("i = %d\n", i);
+    }
+}
